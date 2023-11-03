099b38c56f6c70414a70c03f72c64766967716a2
Nicklas Rudolfsson <66663564+nirucon@users.noreply.github.com>
2023-11-03 19:10:44 +0100

    Update install-arch-pacman-workstation-core.sh

--- install-arch-pacman-workstation-core.sh
+++ install-arch-pacman-workstation-core.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot lxappearance arc-gtk-theme arc-icon-theme pcmanfm arandr nitrogen picom openssh cmus scrot gnupg pass mpv dunst
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot lxappearance arc-gtk-theme arc-icon-theme pcmanfm arandr nitrogen picom openssh cmus scrot gnupg pass mpv dunst maim
